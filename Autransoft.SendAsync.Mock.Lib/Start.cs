aa4303c13fb09a0c2b618bf44d306dfd32272b9e
thiagoautran <thiagoautran@gmail.com>
2021-10-14 23:13:15 -0300

    criada nuget lib

---
+++ Autransoft.SendAsync.Mock.Lib/Start.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendAsyncMock
+{
+    public class Start
+    {
+        public void Add(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
