cf36905b313a74e3631d824a71df4463d0a378bb
thiagoautran <thiagoautran@gmail.com>
2021-10-15 22:45:42 -0300

    adicionada mais opções de setup

--- Autransoft.SendAsync.Mock.Lib/Helpers/GenericMethodHelper.cs
+++ Autransoft.SendAsync.Mock.Lib/Helpers/GenericMethodHelper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq.Expressions;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Autransoft.SendAsync.Mock.Lib.Entities;
@@ -26,6 +27,48 @@ namespace Autransoft.SendAsync.Mock.Lib.Helpers
 
             _mockRepository = mockRepository;
             _expression = expression;
+        }
+
+        public GenericMethodHelper<INTERFACE, CLASS> Setup(HttpMethod requestHttpMethod, string requestAbsolutePath, string requestQuery, HttpStatusCode responseHttpStatusCode, Func<object> responseResponseFunc)
+        {
+            _genericMethodMock.RequestResponseRepository.Add
+            (
+                new RequestEntity
+                {
+                    HttpMethod = requestHttpMethod,
+                    AbsolutePath = requestAbsolutePath,
+                    Query = requestQuery
+                },
+                new ResponseEntity
+                {
+                    HttpStatusCode = responseHttpStatusCode,
+                    ResponseFunc = responseResponseFunc,
+                    ResponseObject = null
+                }
+            );
+
+            return this;
+        }
+
+        public GenericMethodHelper<INTERFACE, CLASS> Setup(HttpMethod requestHttpMethod, string requestAbsolutePath, string requestQuery, HttpStatusCode responseHttpStatusCode, object responseResponseObject)
+        {
+            _genericMethodMock.RequestResponseRepository.Add
+            (
+                new RequestEntity
+                {
+                    HttpMethod = requestHttpMethod,
+                    AbsolutePath = requestAbsolutePath,
+                    Query = requestQuery
+                },
+                new ResponseEntity
+                {
+                    HttpStatusCode = responseHttpStatusCode,
+                    ResponseObject = responseResponseObject,
+                    ResponseFunc = null
+                }
+            );
+
+            return this;
         }
 
         public GenericMethodHelper<INTERFACE, CLASS> Setup(RequestEntity request, ResponseEntity response)
